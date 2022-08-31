e5e92c15f6babfb7a9bdcab9f7dcc73fb652f9ec
hasanhut <hasanhut34@gmail.com>
2022-08-31 19:59:56 +0300

    Add project files.

---
+++ Infrastructure/ECommerceAPI.Persistence/ServiceRegisteration.cs
@@ -0,0 +1,25 @@
+﻿using ECommerceAPI.Application.Abstractions;
+using ECommerceAPI.Persistence.Concretes;
+using ECommerceAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence
+{
+    public static class ServiceRegisteration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            ConfigurationManager configurationManager = new();
+            configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(),"../../Presentation/ECommerceAPI.API"));
+            configurationManager.AddJsonFile("appsettings.json");
+            services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(configurationManager.GetConnectionString("PostgreSQL")));
+        }
+    }
+}
