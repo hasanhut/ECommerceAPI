dd27d607ccc171eb37bb41decf6ebcd303a4a1d5
hasanhut <hasanhut34@gmail.com>
2022-10-14 10:44:58 +0300

    CQRS,FluentValidation

---
+++ Core/ECommerceAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using ECommerceAPI.Application.ViewModels.Products;
+using FluentValidation;
+
+namespace ECommerceAPI.Application.Validators.Products;
+
+public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+{
+    public CreateProductValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Please add product name !!")
+            .MaximumLength(150)
+            .WithMessage("Maximum Characters of Product Name is 150");
+        RuleFor(p => p.Stock)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Please add Stock Information");
+    }
+}
